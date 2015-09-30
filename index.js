48618dd423c3f4eef5eefe404d602adb3297a4b4
Mikko Arminen <mikko.arminen@outlook.com>
2015-09-30 17:39:19 +0000

    read message from a file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
+var filename = 'index.html'
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buf = fs.readFileSync(filename)
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(buf.toString('utf-8'))
 })
 
 app.listen(app.get('port'), function() {
