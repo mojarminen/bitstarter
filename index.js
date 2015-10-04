9a32131815b8cb49550d39927901b43a01a41492
Mikko Arminen <mikko.arminen@outlook.com>
2015-10-04 21:16:08 +0000

    formatting...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var app = express()
 var filename = 'index.html'
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 var buf = fs.readFileSync(filename)
